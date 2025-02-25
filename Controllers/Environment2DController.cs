ea44b81ca24a0b56f068bf832a1334d80634dc1f
Tjeerd Kootwijk <tjeerdgaming@gmail.com>
2025-02-25 13:45:22 +0100

    Added dependacies

--- Controllers/Environment2DController.cs
+++ Controllers/Environment2DController.cs
@@ -1,10 +1,14 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using LU2WebApi.Repositories;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace LU2WebApi.Controllers
 {
     public class Environment2DController : Controller
     {
+        //private readonly IUserRepository _userRepository;
+        private readonly ILogger<Environment2DController> _logger;
+
         public IActionResult Index()
         {
             return View();
